0737aec75877b64aefd88853764f6492b653e74b
smilillo <sofiemilillo@gmail.com>
2023-04-27 22:50:15 -0400

    finished some user routes, added comments for which routes need completed on comment routes

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// get all comments route
+
+// get by id route
+
+// post route
+
+// update by id route
+
+// delete route
+
+// router.get('/', (req, res) => {
+//     Post.findAll({
+//             attributes: ['id', 'title', 'post_text'],
+//             include: [{
+//                 model: User,
+//                 attributes: ['username']
+//             },
+//             {
+//                 model: Comment,
+//                 attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+//                 include: {
+//                     model: User,
+//                     attributes: ['username']
+//                 }
+//             }
+//         ]
+//     })
+//     .then(data => res.json(data))
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     })
+// });
+
+// // get by id route
+
+// router.post('/', withAuth, (req, res) => {
+//     Post.create({
+//       title: req.body,
+//       post_text: req.body.post_text,
+//       user_id: req.session.user_id,
+//     })
+//     .then(data => res.json(data))
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
+
+// // update by id route
+
+// router.delete('/:id', withAuth, (req, res) => {
+//   Project.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     })
+//     .then(data => res.json(data))
+//     .catch (err => {
+//     console.log(err);
+//     res.status(500).json(err);
+//   });
+// });
+
+module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,20 +1,40 @@
-// const router = require('express').Router();
-// const { User } = require('../../models');
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
 
-// router.post('/', async (req, res) => {
-//   try {
-//     const userData = await User.create(req.body);
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: ['username', 'email']
+    })
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
-//     req.session.save(() => {
-//       req.session.user_id = userData.id;
-//       req.session.logged_in = true;
+// get by id route
 
-//       res.status(200).json(userData);
-//     });
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+  .then(data => {
+
+  })
+
+    // req.session.save(() => {
+    //   req.session.user_id = userData.id;
+    //   req.session.logged_in = true;
+
+    //   res.status(200).json(userData);
+    // });
+  .catch (err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 // router.post('/login', async (req, res) => {
 //   try {
@@ -48,14 +68,18 @@
 //   }
 // });
 
-// router.post('/logout', (req, res) => {
-//   if (req.session.logged_in) {
-//     req.session.destroy(() => {
-//       res.status(204).end();
-//     });
-//   } else {
-//     res.status(404).end();
-//   }
-// });
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
-// module.exports = router;
+// update user route
+
+// delete user route
+
+module.exports = router;
