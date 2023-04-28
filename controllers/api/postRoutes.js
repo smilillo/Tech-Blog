d1d4109861b2b72ebe018427e44c38e5fbc33979
smilillo <sofiemilillo@gmail.com>
2023-04-27 22:03:53 -0400

    get route for all posts completed

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,8 +3,28 @@ const { User, Post, Comment } = require('../../models');
 // const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
-    
-})
+    Post.findAll({
+            attributes: ['id', 'title', 'post_text'],
+            include: [{
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
 // router.post('/', withAuth, async (req, res) => {
 //   try {
 //     const newProject = await Project.create({
