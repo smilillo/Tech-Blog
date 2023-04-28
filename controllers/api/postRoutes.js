43f29d3523346ece53261801e6f1ff46d272225d
smilillo <sofiemilillo@gmail.com>
2023-04-27 22:30:55 -0400

    wrote most of the postroutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -25,37 +25,32 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     })
 });
-// router.post('/', withAuth, async (req, res) => {
-//   try {
-//     const newProject = await Project.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
 
-//     res.status(200).json(newProject);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+      title: req.body,
+      post_text: req.body.post_text,
+      user_id: req.session.user_id,
+    })
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//   try {
-//     const projectData = await Project.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
+router.delete('/:id', withAuth, (req, res) => {
+  Project.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    })
+    .then(data => res.json(data))
+    .catch (err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
-//     if (!projectData) {
-//       res.status(404).json({ message: 'No project found with this id!' });
-//       return;
-//     }
-
-//     res.status(200).json(projectData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// module.exports = router;
+module.exports = router;
