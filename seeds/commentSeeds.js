de5a4ff3da88c268377180dc65d7966bba76180c
smilillo <sofiemilillo@gmail.com>
2023-04-27 20:52:55 -0400

    commentseeds done

---
+++ seeds/commentSeeds.js
@@ -0,0 +1,27 @@
+const { Comment } = require('../models');
+
+const commentData = [{
+        comment_text: "",
+        user_id: 1,
+        post_id: 1
+    },
+    {
+        comment_text: "",
+        user_id: 2,
+        post_id: 2
+    },
+    {
+        comment_text: "",
+        user_id: 3,
+        post_id: 3
+    },
+    {
+        comment_text: "",
+        user_id: 4,
+        post_id: 4
+    }
+];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
