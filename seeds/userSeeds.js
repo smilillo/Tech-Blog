b142bb5ccf0886bf689f2144febfb363c493f6da
smilillo <sofiemilillo@gmail.com>
2023-05-01 19:20:27 -0400

    removed email from model and seeds

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -3,27 +3,22 @@ const { User } = require('../models');
 const userData = [
   {
     username: "sofiem",
-    email: "sofiem@email.com",
     password: "password1"
   },
   {
     username: "jaimem",
-    email: "jaimem@email.com",
     password: "password1"
   },
   {
     username: "ruths",
-    email: "ruths@email.com",
     password: "password1"
   },
   {
     username: "joshd",
-    email: "joshd@email.com",
     password: "password1"
   },
   {
     username: "chrisa",
-    email: "chrisa@email.com",
     password: "password1"
   }
 ]
