b1971c45274cfb9aa459596a4dd12e05d0451244
smilillo <sofiemilillo@gmail.com>
2023-04-30 21:24:53 -0400

    updated delete post js

---
+++ public/js/delete-post.js
@@ -0,0 +1,18 @@
+const deleteButtonHandler = async (event) => {
+    // unsure about first if statement
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document.querySelector('.delete-btn').addEventListener('click', deleteButtonHandler);
